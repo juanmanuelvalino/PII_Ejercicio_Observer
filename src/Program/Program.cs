36223879f473e6053b6b6d5dc3a437409b98f00d
Juan Manuel Valiño <jmvdm09@gmail.com>
2018-09-08 21:06:05 -0300

    Observer

---
+++ src/Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            TemperatureReporter reporter = new TemperatureReporter();
+            monitor.Subscribe(reporter);
+            monitor.GetTemperature();
+        }
+    }
+}
